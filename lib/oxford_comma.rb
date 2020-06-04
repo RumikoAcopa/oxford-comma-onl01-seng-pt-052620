fbd77dfbfeb4a21dd48469cf9cd47e6779be3cb0
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-04 17:27:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 2
+
+    return "#{array[0]} and #{array[1]}"
+
+  elsif array.length > 2
+
+    array[array.length-1] = "and " + array[array.length-1]
+
+  end
+  array.join(", ")
 end
+
+# if x > 2
+#    puts "x is greater than 2"
+# elsif x <= 2 and x!=0
+#    puts "x is 1"
+# else
+#    puts "I can't guess the number"
+# end
